af88912c49200556e8467cb68624b9513450d202
Tatsuk1 <t.tatsuki0417@gmail.com>
2019-11-20 23:08:04 +0900

    create reply

--- app/models/thread1.rb
+++ app/models/thread1.rb
@@ -1,5 +1,6 @@
 class Thread1 < ApplicationRecord
 	has_many :comments
+	has_many :replies, class_name: "Comment", foreign_key: :reply_comment, dependent: :destroy
 	validates :name, presence: true, length: { maximum: 30 }
 	validates :description, length: {maximum: 140 }
 	validate :validate_name_not_including_comma
